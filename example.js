14e8302220d417bc8de8b5b2533854b24c8ddd75
Coding-Demon <93517332+Coding-Demon@users.noreply.github.com>
2024-03-25 11:40:13 +0530

    Comments edited for new commit

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 console.log("hello World")
 // This is created for learning the concept of pull request
 // I  am learning git in a very specific way for my office works
-// This comment is for testing purpose.......  --- 1
+// This comment is for testing purpose.......  --- 1001
 
-// This comment is for testing purpose.......  --- 2
+// This comment is for testing purpose.......  --- 2001
 
-// This comment is for testing purpose.......  --- 3
+// This comment is for testing purpose.......  --- 3001
 
-// This comment is for testing purpose.......  --- 4
+// This comment is for testing purpose.......  --- 4001
+
+// This comment is for testing purpose.......  --- 5001
 
